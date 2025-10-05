023c8be77f3bbc7385f1d338ae6959d2f853fb14
brendanharrington <brha8770@colorado.edu>
2025-10-05 17:19:37 -0600

    refactor: changed individual states to one complex state

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,37 +4,37 @@ const Header = () => <h1>Unicafe Feedback Application</h1>
 
 const Subheader = ({text}) => <h2>{text}</h2>
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Statistics = (props) => {
+const Statistics = ({feedback}) => {
+  const {good, neutral, bad} = feedback
   return (
     <div>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
 
-  const incrementGood = () => setGood(good + 1)
-  const incrementNeutral = () => setNeutral(neutral + 1)
-  const incrementBad = () => setBad(bad + 1)
+  const handleFeedback = (type) => { 
+    setFeedback({
+      ...feedback, [type]: feedback[type] + 1
+    })
+  }
 
   return (
     <div>
       <Header />
       <Subheader text={'Give Feedback'} />
-      <Button text={'good'} onClick={incrementGood} />
-      <Button text={'neutral'} onClick={incrementNeutral} />
-      <Button text={'bad'} onClick={incrementBad} />
+      <Button text='Good' onClick={() => handleFeedback('good')} />
+      <Button text='Neutral' onClick={() => handleFeedback('neutral')} />
+      <Button text='Bad' onClick={() => handleFeedback('bad')} />
       <Subheader text={'Statistics'} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics feedback={feedback} />
     </div>
   )
 }
