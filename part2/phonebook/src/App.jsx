287a3c9a9ae97dd520a65a8f9f42cfeec6b7649b
brendanharrington <brha8770@colorado.edu>
2025-10-06 20:31:15 -0600

    feat: completed exercise 2.9
    added functionality to conditionally render people from the phonebook based on a case insensitive name filter stored in state

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-1234567" }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [nameFilter, setNameFilter] = useState('')
+  const personsToShow = showAll
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(nameFilter))
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -29,9 +37,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    if (event.target.value) {
+      setNameFilter(event.target.value.toLowerCase())
+      setShowAll(false)
+    } else {
+      setShowAll(true)
+    }
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <form>
+        Filter by name: <input onChange={handleFilterChange}/>
+      </form>
+      <h2>Add a new number</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} required />
@@ -45,7 +66,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul style={{listStyleType: 'none', paddingInlineStart: 0}}>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {personsToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
