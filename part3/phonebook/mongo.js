facdd8138228ae7d639d538cfeff0648e0f5db8a
brendanharrington <brha8770@colorado.edu>
2025-10-09 02:16:43 -0600

    feat: completed exercise 3.12
    added functionality to connect to cloud-based MongoDB database to add and display users

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+if (process.argv.length > 5) {
+  console.log('too many arguments provided')
+  process.exit(1)
+}
+
+if (process.argv.length === 4) {
+  console.log('give password, name, and number as the arguments')
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://brendanharrington09_db_user:${password}@cluster0.i9ekwzh.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+
+  person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+}
+
