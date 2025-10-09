1bfcea215d3f9b18535db2e8cf771f02dca988da
brendanharrington <brha8770@colorado.edu>
2025-10-09 01:04:44 -0600

    fix: commiting missed files

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req) => {
@@ -9,6 +11,7 @@ morgan.token('body', (req) => {
 })
 
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
+app.use(express.static('dist'))
 
 let persons = [
   { 
@@ -102,7 +105,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
