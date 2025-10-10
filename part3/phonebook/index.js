9aeaf271e605241fe977a52c60c46aaa8c1d443c
brendanharrington <brha8770@colorado.edu>
2025-10-10 16:29:48 -0600

    feat: complete exercise 3.18
    update /info endpoint to show data for the database rather than locally stored dummy array

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -25,35 +25,18 @@ morgan.token('body', (req) => {
 
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`))
 
-let persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${Date()}</p>`)
+  Person.countDocuments({})
+    .then(count => {
+      response.send(
+        `<p>Phonebook has info for ${count} people</p> <p>${Date()}</p>`
+      )
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
