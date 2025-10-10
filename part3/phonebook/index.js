7763d5a5a2a27fb16958a319c3c109262728320e
brendanharrington <brha8770@colorado.edu>
2025-10-10 15:18:47 -0600

    feat: enable fetching entry by id

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -92,14 +92,18 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(404).end()
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
