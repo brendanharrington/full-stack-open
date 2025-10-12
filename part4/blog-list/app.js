7691cb849a89e6d5cb3ab8345986bb0a8223724a
brendanharrington <brha8770@colorado.edu>
2025-10-11 19:29:39 -0600

    test: change URI reference and define tests
    change reference to database and define tests/scripts for querying database

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -7,10 +7,10 @@ const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.TEST_MONGODB_URI)
 
 mongoose
-  .connect(config.MONGODB_URI)
+  .connect(config.TEST_MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
---
+++ part4/blog-list/mongo.js
@@ -0,0 +1,49 @@
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+
+if (process.argv.length > 6) {
+  console.log('too many arguments provided')
+  process.exit(1)
+}
+
+if (process.argv.length === 3 || process.argv.length === 4 || process.argv.length === 5) {
+  console.log('give title, author, url, and likes as the arguments')
+}
+
+const url = config.TEST_MONGODB_URI
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+if (process.argv.length === 2) {
+  console.log('blog list:')
+  Blog.find({}).then(result => {
+    result.forEach(blog => {
+      console.log(`${blog.title} ${blog.author} ${blog.url} ${blog.likes}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const blog = new Blog({
+    title: process.argv[2],
+    author: process.argv[3],
+    url: process.argv[4],
+    likes: Number(process.argv[5]),
+  })
+
+  blog.save().then(result => {
+    console.log('blog saved!')
+    mongoose.connection.close()
+  })
+}
+
