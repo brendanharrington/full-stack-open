1e91dbeaf7b5c586f88037021f41439959502c48
brendanharrington <brha8770@colorado.edu>
2025-10-11 22:35:34 -0600

    feat: add GET and DELETE requests for ids

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -6,6 +6,15 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (note) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
 blogsRouter.post('/', async (request, response) => {
   const { body } = request
 
@@ -18,6 +27,11 @@ blogsRouter.post('/', async (request, response) => {
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(requests.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
