defeaaa4e400ea41dc847e3d91b0698ddf1f436f
brendanharrington <brha8770@colorado.edu>
2025-10-12 21:33:35 -0600

    feat: complete exercise 5.2
    add functionality to log user out, ensuring that localStorage and private token variable are cleared

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -13,14 +13,34 @@ const App = () => {
     blogService.getAll().then(blogs => setBlogs(blogs))
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (username, password) => {
     try {
       const user = await loginService.login({ username, password })
+
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
     } catch {
       setErrorMessage('wrong credentials')
       setTimeout(() => setErrorMessage(null), 5000)
     }
+  }
+
+  const handleLogout = async () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    blogService.setToken(null)
+    setUser(null)
   }
 
   if (!user) {
@@ -38,7 +58,7 @@ const App = () => {
     <div>
       <h1>Blog List Application</h1>
       {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout}>Logout</button></p>
 
       <h2>Blogs</h2>
       {blogs.map(blog => (
