3fdb0dbaa1b987b76707480333b2a303136426fd
brendanharrington <brha8770@colorado.edu>
2025-10-12 20:55:09 -0600

    feat: complete exercise 5.1
    add login functionality to frontend and conditional rendering of blogs depending on if user is logged in

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +16,69 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          Username
+          <input 
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password
+          <input 
+            type="text"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">Login</button>
+    </form>
   )
+
+  if (!user) {
+    return (
+      <div>
+        <h1>Blog List Application</h1>
+        <p>{errorMessage}</p>
+        <h2>Login</h2>
+        {loginForm()}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>Blog List Application</h1>
+        <p>{errorMessage}</p>
+        <h2>Blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
