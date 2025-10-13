9e53c53618cb179e271887fc7a17800593584595
brendanharrington <brha8770@colorado.edu>
2025-10-13 13:47:31 -0600

    test: complete exercise 5.14
    add tests for Togglable component and Blog component to verify that URL and likes elements are rendered after clicking the button to toggle visibility

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,15 +1,16 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  test('only renders title and author by default', () => {
-    const blog = {
-      title: 'Component testing is done with react-testing-library',
-      author: 'Brendan',
-      url: 'www.testing.com',
-      likes: 18
-    }
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Brendan',
+    url: 'www.testing.com',
+    likes: 18
+  }
 
+  test('only renders title and author by default', () => {
     render(<Blog blog={blog} />)
 
     const titleElement = screen.getByText(blog.title, { exact: false })
@@ -21,5 +22,19 @@ describe('<Blog />', () => {
     expect(authorElement).toBeDefined()
     expect(urlElement).toBeNull()
     expect(likesElement).toBeNull()
+  })
+
+  test('renders url and number of likes after toggling', async () => {
+    const { container } = render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = container.querySelector('.blog-toggle-btn')
+    await user.click(button)
+
+    const urlElement = screen.queryByText(blog.url)
+    const likesElement = screen.queryByText(`likes ${blog.likes}`)
+
+    expect(urlElement).toBeDefined()
+    expect(likesElement).toBeDefined()
   })
 })
