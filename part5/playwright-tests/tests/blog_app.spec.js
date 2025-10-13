f98380d8fc65faf2350c2089c8e0775c6d203f55
brendanharrington <brha8770@colorado.edu>
2025-10-13 17:30:57 -0600

    test: complete exercise 5.18
    add functionality to clear the database and add a new user each time a test is run, adjusting the backend as needed

--- part5/playwright-tests/tests/blog_app.spec.js
+++ part5/playwright-tests/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Brendan Harrington',
+        username: 'brendanharrington',
+        password: 'password'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -15,5 +24,36 @@ describe('Blog app', () => {
 
     const locator = page.getByRole('heading', { name: 'Login' })
     await expect(locator).toBeVisible()
+  })
+  
+  test('form is shown after clicking login button', async ({ page }) => {
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    const locator = page.getByRole('heading', { name: 'Login' })
+    await expect(locator).toBeVisible()
+  })
+
+  describe('Login', () => {
+    beforeEach( async ({ page }) => {
+      await page.getByRole('button', { name: 'Login' }).click()
+    })
+
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('Username').fill('brendanharrington')
+      await page.getByLabel('Password').fill('password')
+      await page.getByRole('button', { name: 'Login' }).click()
+      
+      const locator = page.getByText('Brendan Harrington logged in')
+      await expect(locator).toBeVisible()
+    })
+
+    test('fails with incorrect credentials', async ({ page }) => {
+      await page.getByLabel('Username').fill('brendanharrington')
+      await page.getByLabel('Password').fill('wrongpassword')
+      await page.getByRole('button', { name: 'Login' }).click()
+      
+      const locator = page.getByText('Wrong credentials')
+      await expect(locator).toBeVisible()
+    })
   })
 })
