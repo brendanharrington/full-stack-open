a0f202590d6a5f720a9187d68092ff02f78c288e
brendanharrington <brha8770@colorado.edu>
2025-10-12 21:12:02 -0600

    refactor: define LoginForm component
    create LoginForm component and isolate state variables for username and password

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,84 +1,51 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [errorMessage, setErrorMessage] = useState(null)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then(blogs => setBlogs(blogs))
   }, [])
 
-  const handleLogin = async event => {
-    event.preventDefault()
-
+  const handleLogin = async (username, password) => {
     try {
       const user = await loginService.login({ username, password })
       setUser(user)
-      setUsername('')
-      setPassword('')
     } catch {
       setErrorMessage('wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+      setTimeout(() => setErrorMessage(null), 5000)
     }
   }
-
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        <label>
-          Username
-          <input 
-            type="text"
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Password
-          <input 
-            type="text"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </label>
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  )
 
   if (!user) {
     return (
       <div>
         <h1>Blog List Application</h1>
-        <p>{errorMessage}</p>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         <h2>Login</h2>
-        {loginForm()}
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <h1>Blog List Application</h1>
-        <p>{errorMessage}</p>
-        <h2>Blogs</h2>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
+        <LoginForm onLogin={handleLogin} />
       </div>
     )
   }
+
+  return (
+    <div>
+      <h1>Blog List Application</h1>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      <p>{user.name} logged in</p>
+
+      <h2>Blogs</h2>
+      {blogs.map(blog => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  )
 }
 
 export default App
---
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const LoginForm = ({ onLogin }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    onLogin(username, password)
+    setUsername('')
+    setPassword('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Username
+          <input 
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password
+          <input 
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  )
+}
+
+export default LoginForm
