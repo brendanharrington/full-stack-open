1fc6e8e55a5b2ff9a31c6a5e39b7328449f1702f
brendanharrington <brha8770@colorado.edu>
2025-10-15 00:09:32 -0600

    feat: complete exercises 6.14-6.15
    use fetch API to make GET and POST requests to the backend server with json-server, saving new anecdotes to db.json

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,20 +1,22 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const create = event => {
+  const addAnecdote = async event => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
   }
 
   return (
     <div>
       <h2>create new</h2>
-      <form onSubmit={create}>
+      <form onSubmit={addAnecdote}>
         <div>
           <input name='anecdote' />
         </div>
