9d41abfaaabc5c0707c8380dd55cb5473e14349b
brendanharrington <brha8770@colorado.edu>
2025-10-14 22:59:41 -0600

    refactor: complete exercises 6.10-6.11
    use @reduxjs/toolkit to simplify state management for storing anecdotes and filter in state

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit" 
+
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -19,40 +21,24 @@ const asObject = anecdote => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const anecdoteReducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  switch(action.type) {
-    case 'VOTE': {
-      const { id } = action.payload
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      const content = action.payload
+      state.push(asObject(content))
+    },
+    vote(state, action) {
+      const id = action.payload
       const anecdoteToChange = state.find(a => a.id === id)
       const changedAnecdote = {
         ...anecdoteToChange, votes: anecdoteToChange.votes + 1
       }
       return state.map(a => (a.id !== id ? a : changedAnecdote))
     }
-    case 'NEW_ANECDOTE': {
-      const { content } = action.payload
-      return [...state, asObject(content)]
-    }
-    default:
-      return state
   }
-}
+})
 
-export const vote = id => {
-  return {
-    type: 'VOTE', 
-    payload: { id }
-  }
-}
-
-export const createAnecdote = content => {
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: { content }
-  }
-}
-
-export default anecdoteReducer
+export const { createAnecdote, vote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
