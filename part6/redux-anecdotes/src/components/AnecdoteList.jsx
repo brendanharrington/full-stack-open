91c0f232d7fdd687025c9a302ae05065b6bcc00e
brendanharrington <brha8770@colorado.edu>
2025-10-14 23:24:45 -0600

    feat: complete exercises 6.12-6.13
    add functionality to show notifications after liking an anecdote, hiding the message after 5 seconds

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,10 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
+import { showNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector(state => state.anecdotes)
   const filter = useSelector(state => state.filter)
+
+  const handleClick = anecdote => {
+    dispatch(vote(anecdote.id))
+    dispatch(showNotification(`You voted for "${anecdote.content}"`))
+    setTimeout(() => {
+      dispatch(showNotification(''))
+    }, 5000)
+  }
 
   return (
     <div>
@@ -17,7 +26,7 @@ const AnecdoteList = () => {
             <div>{anecdote.content}</div>
             <div>
               has {anecdote.votes}
-              <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+              <button onClick={() => handleClick(anecdote)}>vote</button>
             </div>
           </div>
       ))}
