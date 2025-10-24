bf08fbf7f4ebc53b5582e943a4abdbb7cfc12711
brendanharrington <brha8770@colorado.edu>
2025-10-23 19:49:49 -0600

    feat: complete exercise 9.5
    add endpoint for bmiCalculator module, addressing error handling correctly

--- part9/first-steps/bmiCalculator.ts
+++ part9/first-steps/bmiCalculator.ts
@@ -12,20 +12,25 @@ const calculateBmi = (heightInCm: number, weightInKg: number) => {
   }
 }
 
-const args = process.argv.slice(2);
-if (args.length < 2) {
-  throw new Error('Not enough arguments. Usage: <heightInCm> <weightInKg>');
+if (require.main === module) {
+  const args = process.argv.slice(2);
+  if (args.length < 2) {
+    throw new Error('Not enough arguments. Usage: <heightInCm> <weightInKg>');
+  }
+  
+  const [heightArg, weightArg] = args;
+  const heightInCm: number = Number(heightArg);
+  const weightInKg: number = Number(weightArg);
+  
+  if (isNaN(heightInCm) || isNaN(weightInKg)) {
+    throw new Error('Provided values were not numbers!');
+  }
+  if (heightInCm <= 0 || weightInKg <= 0) {
+    throw new Error('Height and weight must be positive numbers');
+  }
+  
+  console.log(calculateBmi(heightInCm, weightInKg))
 }
 
-const [heightArg, weightArg] = args;
-const heightInCm: number = Number(heightArg);
-const weightInKg: number = Number(weightArg);
 
-if (isNaN(heightInCm) || isNaN(weightInKg)) {
-  throw new Error('Provided values were not numbers!');
-}
-if (heightInCm <= 0 || weightInKg <= 0) {
-  throw new Error('Height and weight must be positive numbers');
-}
-
-console.log(calculateBmi(heightInCm, weightInKg))
+export default calculateBmi
