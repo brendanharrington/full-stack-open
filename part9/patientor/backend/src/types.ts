9ad9008ffe4e27692f7b7fe4d7ee32fc6ddc26e7
brendanharrington <brha8770@colorado.edu>
2025-10-27 20:20:17 -0600

    feat: complete exercise 9.24
    update patient dataset to expanded version and fix typing file accordingly

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -1,9 +1,6 @@
 import z from 'zod';
 import { newPatientSchema } from './utils';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
-
 export interface Diagnosis {
   code: string;
   name: string;
@@ -21,6 +18,51 @@ export interface Patient extends NewPatient {
   entries: Entry[];
 }
 
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
