fc309f24c0bc6db37a7d3b08b98e42f8301fe142
brendanharrington <brha8770@colorado.edu>
2025-11-13 16:19:44 -0700

    feat: update frontend to display blogs and form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+const blogs = [
+  {
+    title: 'Title 0',
+    author: 'Author 0',
+    url: 'www.example-0.com'
+  },
+  {
+    title: 'Title 1',
+    author: 'Author 1',
+    url: 'www.example-1.com'
+  },
+  {
+    title: 'Title 2',
+    author: 'Author 2',
+    url: 'www.example-2.com'
+  }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [blogs, setBlogs] = useState([]);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTitle('');
+    setAuthor('');
+    setUrl('');
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Blog List Application</h1>
+      <h2>Add a Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          title
+          <input 
+            type='text'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          author
+          <input 
+            type='text'
+            value={author}
+            onChange={e => setAuthor(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          url
+          <input 
+            type='text'
+            value={url}
+            onChange={e => setUrl(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type='submit'>add</button>
+      </form>
+      <div>{title}</div>
+      <div>{author}</div>
+      <div>{url}</div>
+      <h2>Blogs</h2>
+      <table style={{ width: '100%', textAlign: 'center', border: 'solid' }}>
+        <thead style={{ backgroundColor: 'lightgrey' }}>
+          <th scope='col'>Title</th>
+          <th scope='col'>Author</th>
+          <th scope='col'>URL</th>
+        </thead>
+        <tbody>
+          {blogs.map(b => (
+            <tr>
+              <td>{b.title}</td>
+              <td>{b.author}</td>
+              <td>{b.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
