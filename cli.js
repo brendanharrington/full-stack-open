6e40dd791c911fa7dabbd6e4ca8b3ead85e22e77
brendanharrington <brha8770@colorado.edu>
2025-11-13 14:28:29 -0700

    feat: complete exercise 13.3
    add cli.js script to formatted contents of the database to the command line

---
+++ cli.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { Sequelize, QueryTypes } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const main = async () => {
+  try {
+    await sequelize.authenticate();
+    const blogs = await sequelize.query(
+      'SELECT * FROM blogs',
+      { type: QueryTypes.SELECT }
+    );
+    
+    blogs.forEach(b => {
+      console.log(`${b.author ?? 'unknown'}: '${b.title}', ${b.likes} likes`);
+    });
+
+    sequelize.close();
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+main();
