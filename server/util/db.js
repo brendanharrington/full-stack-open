8454128006506a3dbbcdea571df6cf994beb4af6
brendanharrington <brha8770@colorado.edu>
2025-11-13 21:40:16 -0700

    refactor: move database connection logic to its own file

---
+++ server/util/db.js
@@ -0,0 +1,17 @@
+import { Sequelize } from 'sequelize';
+import { DATABASE_URL } from './config';
+
+const sequelize = new Sequelize(DATABASE_URL);
+
+export const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('connected to the database');
+  } catch (error) {
+    console.log('failed to connect to the database');
+    console.log('error:', error);
+    return process.exit(1);
+  }
+
+  return null;
+};
